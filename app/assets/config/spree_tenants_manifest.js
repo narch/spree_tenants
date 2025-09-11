a94a123a2b28f83e09bc7e85821e6aebb9d48019
Nathan Williams <nathan@nathan.la>
2025-09-11 11:18:26 -0700

    remove reference to non existent folders in minifest

--- app/assets/config/spree_tenants_manifest.js
+++ app/assets/config/spree_tenants_manifest.js
@@ -1,5 +1,3 @@
 //= link_tree ../images
 //= link spree_tenants/application.js
 //= link_tree ../../javascript/spree_tenants/controllers .js
-//= link_tree ../../../vendor/javascript .js
-//= link_tree ../../../vendor/stylesheets .css
